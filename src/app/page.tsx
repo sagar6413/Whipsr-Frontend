b31d5500571967d7be73bac805edbd9107c4072e
Sagar Shrivastava <sagar.shrivastava6413@gmail.com>
2025-04-30 17:00:06 +0530

    0.1.9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,20 @@ import Features from "@/components/layout/Features";
 import CallToAction from "@/components/layout/CallToAction";
 import FaqSection from "@/components/layout/FaqSection";
 import Footer from "@/components/layout/Footer";
+import { isAuthenticated } from "@/utils/cookieManager";
 import { useUserStore } from "@/store/userStore";
 
 export default function Home() {
-  const { isAuthenticated, user } = useUserStore();
-
   // State/Effects related to the entire page structure or shared across sections
   // can remain here if absolutely necessary, but most have been moved.
+  const { user } = useUserStore();
 
   return (
     // Use arbitrary value for the specific dark background
     <div className="min-h-screen bg-[#121212] text-white">
       <Navbar
         navLinks={navLinks}
-        isAuthenticated={isAuthenticated}
+        isAuthenticated={isAuthenticated()}
         user={user}
       />
 
